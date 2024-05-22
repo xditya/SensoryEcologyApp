0b7c41c0815c0468530784e109c4a480ab3f161f
Aditya <me@xditya.me>
2024-05-22 21:17:37 +0530

    feat: homescreen data?

--- app/src/main/java/me/xditya/sensoryecology/ui/screens/HomeScreen.kt
+++ app/src/main/java/me/xditya/sensoryecology/ui/screens/HomeScreen.kt
@@ -1,20 +1,30 @@
 package me.xditya.sensoryecology.ui.screens
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.Image
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.border
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.layout.size
+import androidx.compose.foundation.layout.wrapContentSize
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavHostController
 import me.xditya.sensoryecology.R
 import me.xditya.sensoryecology.ui.components.MyBottomAppBar
@@ -23,6 +33,20 @@ import me.xditya.sensoryecology.ui.components.MyTopAppBar
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen(navController: NavHostController) {
+    val rainbowColorsBrush = remember {
+        Brush.sweepGradient(
+            listOf(
+                Color(0xFF9575CD),
+                Color(0xFFBA68C8),
+                Color(0xFFE57373),
+                Color(0xFFFFB74D),
+                Color(0xFFFFF176),
+                Color(0xFFAED581),
+                Color(0xFF4DD0E1),
+                Color(0xFF9575CD)
+            )
+        )
+    }
     Scaffold(
         topBar = { MyTopAppBar(navController, "Sensory Ecology") },
         bottomBar = { MyBottomAppBar(navController = navController) }
@@ -33,26 +57,32 @@ fun HomeScreen(navController: NavHostController) {
                 .padding(it)
                 .fillMaxSize()
         ) {
-            Box(
-                modifier = Modifier.fillMaxSize(),
-                contentAlignment = Alignment.Center
+            Column(
+                modifier = Modifier
+                    .fillMaxSize()
+                    .padding(20.dp),
+                horizontalAlignment = Alignment.CenterHorizontally
             ) {
-                LazyColumn(
-                    modifier = Modifier.fillMaxSize(),
-                    horizontalAlignment = Alignment.CenterHorizontally,
-                    verticalArrangement = Arrangement.Center
-                ) {
-                    item {
-                        Column(
-                            Modifier.padding(8.dp)
-                        ) {
-                            Image(
-                                painterResource(id = R.drawable.main),
-                                contentDescription = "main image"
-                            )
-                        }
-                    }
-                }
+                Image(
+                    painter = painterResource(id = R.drawable.main),
+                    contentDescription = "main image",
+                    modifier = Modifier
+                        .size(250.dp)
+                        .wrapContentSize(Alignment.Center)
+                        .clip(RoundedCornerShape(15.dp))
+                        .border(
+                            BorderStroke(4.dp, rainbowColorsBrush),
+                            RoundedCornerShape(15.dp)
+                        )
+                )
+                Spacer(modifier = Modifier.padding(18.dp))
+                Text(
+                    text = "Crafting Environments: Illuminating, Regulating, and Oxygenating for Optimal Experience.",
+                    style = MaterialTheme.typography.bodyMedium,
+                    fontSize = 15.sp,
+                    color = Color.Gray,
+                    modifier = Modifier.padding(bottom = 16.dp)
+                )
             }
         }
     }
